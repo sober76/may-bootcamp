383c4ba1fa63e09ef86bfb03b69124546e5d78fa
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2025-05-31 20:49:31 +0530

    alb htps

--- class6/infra/alb.tf
+++ class6/infra/alb.tf
@@ -55,10 +55,26 @@ resource "aws_lb_target_group" "alb" {
   }
 }
 
+# http listener for alb
 resource "aws_lb_listener" "http_forward" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb.arn
+  }
+}
+
+# create a https listenr   -> for alb
+
+resource "aws_lb_listener" "https_forward" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.cert.arn
 
   default_action {
     type             = "forward"
--- class6/infra/route53.tf
+++ class6/infra/route53.tf
@@ -17,3 +17,39 @@ resource "aws_route53_record" "app" {
     evaluate_target_health = true
   }
 }
+
+
+# SSL  certificate _> AWS cert manager
+# # Create an ACM certificate
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "${var.environment}.${data.aws_route53_zone.main.name}"
+  validation_method = "DNS"
+
+  tags = {
+    Name = "${var.environment}.${data.aws_route53_zone.main.name}"
+  }
+}
+# # Create a DNS validation record
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+
+# # Validate the ACM certificate
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+
