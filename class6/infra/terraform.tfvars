76a9f51c697e8a45bf4c14a6f4318e467a7bb35f
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2025-05-31 20:03:10 +0530

    addintonds

--- class6/infra/terraform.tfvars
+++ class6/infra/terraform.tfvars
@@ -1,1 +1,13 @@
+# .tfvars
 vpc_cidr = "10.0.0.0/16"
+app_name = "student-portal"
+
+
+subnet_cidr = {
+  "private_1"   = "10.0.1.0/24"
+  "private_2"   = "10.0.2.0/24"
+  "public_1"    = "10.0.3.0/24"
+  "public_2"    = "10.0.4.0/24"
+  "db_subnet_1" = "10.0.5.0/24"
+  "db_subnet_2" = "10.0.6.0/24"
+}
--- class6/infra/variables.tf
+++ class6/infra/variables.tf
@@ -73,3 +73,17 @@ variable "desired_container_count" {
   default     = 2
 
 }
+
+variable "subnet_cidr" {
+  type = map(string)
+
+  default = {
+    "prvate_1"    = "10.0.1.0/24"
+    "prvate_2"    = "10.0.2.0/24"
+    "public_1"    = "10.0.3.0/24"
+    "public_2"    = "10.0.4.0/24"
+    "db_subnet_1" = "10.0.1.0/24"
+    "db_subnet_2" = "10.0.1.0/24"
+  }
+
+}
