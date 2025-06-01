c73d64bc79291d82b6ac21750de991fd61a5a967
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2025-06-01 20:07:06 +0530

    dynmanci secret and workflow

--- class6/infra/locals.tf
+++ class6/infra/locals.tf
@@ -10,7 +10,28 @@ locals {
     environment                   = var.environment
     db_host                       = aws_db_instance.postgres.address
   }
+
+  app_deploy_vars = {
+    AWS_REGION             = var.region
+    APP_ENVIRONMENT        = var.environment
+    IMAGE_REPO_NAME        = aws_ecr_repository.student_portal_app.repository_url
+    ECS_APP_CONTAINER_NAME = var.container_name
+    ECS_TASK_DEFINITION    = "${var.environment}-${var.app_name}"
+    ECS_SERVICE            = "${var.environment}-${var.app_name}-service"
+    ECS_CLUSTER            = aws_ecs_cluster.main.id
+  }
 }
+
+resource "aws_secretsmanager_secret" "app_deploy_data" {
+  name        = "${var.environment}-${var.app_name}-deploy-data"
+  description = "Deployment data for the student portal app"
+}
+
+resource "aws_secretsmanager_secret_version" "app_deploy_data_version" {
+  secret_id     = aws_secretsmanager_secret.app_deploy_data.id
+  secret_string = jsonencode(app_deploy_vars)
+}
+
 
 
 # "postgresql://${aws_db_instance.postgres.username}:${random_password.dbs_random_string.result}@${aws_db_instance.postgres.address}:${aws_db_instance.postgres.port}/${aws_db_instance.postgres.db_name}"
