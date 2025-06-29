af978af31acfcd832c09b1d5e95ef2a77dfafd04
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2025-06-29 20:01:59 +0530

    rds version fix

--- class12/infra/varibles.tf
+++ class12/infra/varibles.tf
@@ -21,7 +21,7 @@ variable "db_default_settings" {
   default = {
     allocated_storage       = 30
     max_allocated_storage   = 50
-    engine_version          = "14.5"
+    engine_version          = "14.15"
     instance_class          = "db.t3.micro"
     backup_retention_period = 2
     db_name                 = "postgres"
